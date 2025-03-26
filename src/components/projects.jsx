63e57f2299e056c0af9e66ea115f3318adea741d
Alejandro Sanchez <alejandro>
2025-03-26 18:17:46 -0500

    Updated project component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -9,16 +9,18 @@ const Projects = () => {
             <div className="flex flex-col space-y-8 mt-12 text-start">
                 <h2 className="title-section">Projects</h2>
                 <div className="flex flex-col items-center space-y-4">
-                    <div className="flex flex-col w-full bg-darkBlue p-4 rounded-lg md:flex-row-reverse md:space-x-4 md:space-y-0">
-                        <div className="flex flex-col justify-end md:h-auto md:w-1/2 p-0 md:p-4">
+                    
+                    <div className="flex flex-col w-full bg-darkBlue p-4 rounded-lg   md:space-y-0">
+                        <h3 className="text-2xl text-white font-bold hover:text-grayishBlue md:p-2">Bookmark Project</h3>
+                        <div className="flex flex-col md:flex-row-reverse w-full mx-auto mt-4">
+                        <div className="flex flex-col md:mt-8 md:h-auto md:w-1/2 p-0 md:p-2">
                             <img src={bookmarkImg} alt="bookmark-projext" srcset="" className="object-contain rounded-lg"/>
-                            <div className="flex flex-row space-x-4 w-full mt-4 md:mt-8">
+                            <div className="flex flex-row space-x-4 w-full mt-4">
                                 <a href="https://github.com/Alejosz1902/bookmark-manager" className="project-button" target="_blank" rel="noreferrer"><SiGithub/><span>Repositorie</span></a>
                                 <a href="https://bookmark-manager-rose.vercel.app/" className="project-button" target="_blank" rel="noreferrer"><CgWebsite/><span>Live view</span></a>
                             </div>
                         </div>
-                        <div className="flex flex-col w-full py-4 px-2 md:w-1/2 mx-auto">
-                            <h3 className="text-2xl text-white font-bold hover:text-grayishBlue">Bookmark Project</h3>
+                        <div className="flex flex-col w-full md:w-1/2 items-start md:p-2">
                             <p className="text-white mt-4 md:mt-8">This landing page is the final project of my Tailwind CSS certification course. It showcases a clean and fully responsive UI for a bookmark manager web application, built from the ground up using Tailwind CSS best practices. Throughout this project, I applied the utility-first approach to rapidly build and style components, while reinforcing principles like mobile-first design, semantic structure, and UI consistency. The result is a simple yet effective user interface that adapts seamlessly across devices, demonstrating my ability to translate design into functional, scalable code. This project reflects not only my growing proficiency in Tailwind, but also my commitment to learning by doing—because you learn to code by coding, and you get better at coding by practice.</p>
                             <div className="flex flex-row mt-4 mx-0 text-white md:mt-8">
                                 
@@ -36,6 +38,8 @@ const Projects = () => {
                                 </div>
                             </div>
                         </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
